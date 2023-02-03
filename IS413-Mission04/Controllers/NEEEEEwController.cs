d6018a4554211163bf57dd8a1742b5af597c2a66
jel14t <jay.lowry350@gmail.com>
2023-02-03 13:12:14 -0700

    Halfway done with MVC

---
+++ IS413-Mission04/Controllers/NEEEEEwController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IS413_Mission04.Controllers
+{
+    public class NEEEEEwController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+    }
+}
